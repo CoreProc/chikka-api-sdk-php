2567fd4cf8f1c5db77790d116c0239822ca8bb04
Chris Bautista <chrisbjr@gmail.com>
2015-08-24 17:15:31 +0800

    Added a logger for logging

--- src/ChikkaClient.php
+++ src/ChikkaClient.php
@@ -3,6 +3,7 @@
 namespace Coreproc\Chikka;
 
 use GuzzleHttp\Client;
+use Psr\Log\LoggerInterface;
 
 class ChikkaClient
 {
@@ -23,23 +24,42 @@ class ChikkaClient
     private $shortCode;
 
     /**
+     * @var LoggerInterface
+     */
+    public $logger;
+
+    /**
      * @var
      */
     public $client;
 
     /**
+     * Base URL of the Chikka API
+     */
+    const BASE_URL = 'https://post.chikka.com/smsapi/';
+
+    public $loggingEnabled = false;
+
+    /**
      * @param $clientId
      * @param $secretKey
      * @param $shortCode
+     * @param LoggerInterface $logger
      */
-    public function __construct($clientId, $secretKey, $shortCode)
+    public function __construct($clientId, $secretKey, $shortCode, LoggerInterface $logger = null)
     {
-        $this->clientId  = $clientId;
+        $this->clientId = $clientId;
         $this->secretKey = $secretKey;
         $this->shortCode = $shortCode;
 
+        if ( ! empty($logger)) {
+            $this->logger = $logger;
+            $this->loggingEnabled = true;
+        }
 
-        $this->client = new Client();
+        $this->client = new Client([
+            'base_uri' => self::BASE_URL
+        ]);
     }
 
     /**
